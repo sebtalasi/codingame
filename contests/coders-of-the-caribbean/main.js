054b3f787924c3bdaa9fc2db90a11638c57462d3
Sébastien TALASI <Sebastien.TALASI@canal-plus.com>
2017-04-19 22:21:20 +0200

    Bronze !

--- contests/coders-of-the-caribbean/main.js
+++ contests/coders-of-the-caribbean/main.js
@@ -61,7 +61,7 @@
          return this.type + ' ' + this.id + ' ' + this.position.toString();
      }
      getDistance(coord) {
-         return Math.sqrt(Math.pow(this.x - coord.x, 2) + Math.pow(this.y - coord.y, 2));
+         return Math.sqrt(Math.pow(this.position.x - coord.x, 2) + Math.pow(this.position.y - coord.y, 2));
      }
  }
  /**
@@ -77,45 +77,134 @@
          this.action = '';
          this.mine = 0;
          this.fire = 0;
+         this.closestBarrel = {};
+         this.closestFoo = {};
      }
      toString() {
          return super.toString() + ' Speed : ' + this.speed;
      }
- };
- Ship.prototype.play = function () {
-     var thisShip = this;
-     printErr(this, 'play');
-     //    if (this.action == '') {
-     // Decrease reload
-     this.fire = Math.max(0, this.fire - 1);
-     this.mine = Math.max(0, this.mine - 1);
+     evaluate() {
+         var thisShip = this;
 
-     // If barrels, go to closest
-     if (aBarrels.length > 0) {
-         aBarrels.sort(function (a, b) {
+         // Sort barrels
+         //         aBarrels.sort(function (a, b) {
+         //             return thisShip.getDistance(a.position) - thisShip.getDistance(b.position);
+         //         });
+         //printErr(this.id, 'Closest Barrel', aBarrels[0]);
+         //this.closestBarrel = aBarrels[0];
+         // Sort ships
+         /*aShips.sort(function (a, b) {
              return thisShip.getDistance(a.position) - thisShip.getDistance(b.position);
-         });
+         });*/
+     };
+     simPos(turns) {
+         // 1 + (distance to target) / 3
+         var coord = new Coord(this.position.x, this.position.y);
+         while (turns > 0) {
+             // Switch odd vs even
+             switch (coord.x % 2) {
+                 case 1:
+                     switch (this.rotation) {
+                         case 0:
+                             coord.x++;
+                             break;
+                         case 1:
+                             coord.x++;
+                             coord.y--;
+                             break;
+                         case 2:
+                             coord.y--;
+                             break;
+                         case 3:
+                             coord.x--;
+                             break;
+                         case 4:
+                             coord.y++;
+                             break;
+                         case 5:
+                             coord.y++;
+                             coord.x++;
+                     }
+                     break;
+                 case 0:
+                     switch (this.rotation) {
+                         case 0:
+                             coord.x++;
+                             break;
+                         case 1:
+                             coord.y--;
+                             break;
+                         case 2:
+                             coord.y--;
+                             coord.x--;
+                             break;
+                         case 3:
+                             coord.x--;
+                             break;
+                         case 4:
+                             coord.y++;
+                             coord.x--;
+                             break;
+                         case 5:
+                             coord.y++;
+                     }
+                     break;
+             };
+             turns--;
+         };
+     }
+     play() {
+         var thisShip = this;
+         printErr(this, 'play');
+         printErr(aShips);
+         this.action = 'WAIT';
 
-         this.action = new Action('MOVE', aBarrels[0].position);
-     } else {
-         //aShips.sort(function (a, b) {
-         //    return Tools.dist(thisShip.x, thisShip.y, a.x, a.y) - Tools.dist(thisShip.x, thisShip.y, b.x, b.y);
-         //});
+         //    if (this.action == '') {
+         // Decrease reload
+         this.fire = Math.max(0, this.fire - 1);
+         this.mine = Math.max(0, this.mine - 1);
 
-         aShips.forEach(function (ship) {
-             printErr('Fire : ', this.fire);
-             if (ship.owner == 0 && this.getDistance(ship.position) < 8 && this.fire == 0) {
-                 this.action = new Action('FIRE', ship.position);
-             } else if (ship.owner == 0) {
-                 this.action = new Action('MOVE', ship.position);
+
+
+         // If barrels, go to closest
+         if (aBarrels.length > 0) {
+             aBarrels.sort(function (a, b) {
+                 return thisShip.getDistance(a.position) - thisShip.getDistance(b.position);
+             });
+             //  don't get stuck 
+             if (this.speed == 0) {
+                 this.action = new Action('MOVE', aBarrels[1].position);
+             } else {
+                 this.action = new Action('MOVE', aBarrels[0].position);
              }
-         }, this);
+
+
+             // Loof for a static foo to shoot it
+             aShips.forEach(function (ship) {
+                 printErr('Fire dist', this.id, ship.id, ship.position, this.getDistance(ship.position))
+                 if (ship.owner == 0 && this.getDistance(ship.position) <= 3) {
+                     this.action = new Action('FIRE', ship.position);
+                 }
+             }, this);
+         } else {
+             //aShips.sort(function (a, b) {
+             //    return Tools.dist(thisShip.x, thisShip.y, a.x, a.y) - Tools.dist(thisShip.x, thisShip.y, b.x, b.y);
+             //});
+             aShips.forEach(function (ship) {
+                 if (ship.owner == 0 && this.getDistance(ship.position) <= 5 && this.fire == 0) {
+                     this.action = new Action('FIRE', ship.position);
+                 } else if (ship.owner == 0) {
+                     this.action = new Action('MOVE', ship.position);
+                 }
+             }, this);
+         }
+         if (this.action.action == 'FIRE') {
+             this.fire = fireReload;
+         } else if (this.action.action == 'MINE') {
+             this.mine = mineReload;
+         }
+         return this.action;
      }
-     if (this.action.action == 'FIRE') {
-         this.fire = fireReload;
-     }
-     printErr(this.action);
-     return this.action;
  };
 
  /**
@@ -146,7 +235,7 @@
      constructor(id, x, y) {
          super(id, 'CANNONBALL', x, y);
      }
- }
+ };
 
 
  /**
@@ -202,10 +291,13 @@
      //printErr(aBarrels);
      //printErr(aShips);
      aShips.forEach(function (ship) {
+         ship.simPos(2);
+     });
+     aShips.forEach(function (ship) {
          if (ship.owner == 1) {
              print(ship.play());
          }
-     })
+     });
  }
  /**
   * Reset game turn
@@ -214,6 +306,7 @@
      aBarrels = [];
      aGrid = [];
      aMines = [];
+     aShips = [];
 
      // Init hexagonal grid 23 cells wide and 21 high.
      for (var q = 0; q < 23; q++) {
